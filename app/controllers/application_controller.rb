f798adc9e35e547cafda9c32212e1cf3afebb70d
Alexey Kozlov <odinale@ukr.net>
2021-08-09 09:43:37 +0300

    Added DarkTheme to app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
 	http_basic_authenticate_with name: "admin", password: "qwerty"
+
+	before_action :set_theme
+
+	def set_theme
+		if params[:theme].present?
+			theme = params[:theme].to_sym
+			# session[:theme] = theme
+			cookies[:theme] = theme
+			redirect_to(request.referrer || home_path)
+		end
+	end
 end
