815e3577a3dad1b9bb87e85dcabac6caad91460b
Alexey Kozlov <odinale@ukr.net>
2021-08-10 10:55:57 +0300

    Edited ValidateMessages & Calculated TotalCost

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-	validates :total_cost, presence: true, numericality: { greater_than_or_equal_to: 0 }
+	# validates :total_cost, presence: true, numericality: { greater_than_or_equal_to: 0 }
 	validates :daily_menu_id, presence: true
 
 	belongs_to :daily_menu
@@ -7,10 +7,12 @@ class Order < ApplicationRecord
 	accepts_nested_attributes_for :order_items, allow_destroy: true
 	validates_associated :order_items
 
-	after_validation :calc_total_cost
+	after_validation :set_total_cost
 
-	def calc_total_cost
-		order_items.sum(:cost)
+	def set_total_cost
+		self.total_cost = 0
+		order_items.each do |item|
+			self.total_cost += item.cost unless item.marked_for_destruction?
+		end
 	end
-
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,25 +1,26 @@
 class OrderItem < ApplicationRecord
-	validates :daily_menu_item_id, presence: false
+	validates :daily_menu_item_id, presence: false, :uniqueness => { scope: :order_id }
 	validates :quantity_por, presence: false, numericality: { greater_than_or_equal_to: 1 }, unless: :quantity_vol
 	validates :quantity_vol, presence: false, numericality: { greater_than_or_equal_to: 100 }, unless: :quantity_por
-	validates :cost, presence: false, numericality: { greater_than_or_equal_to: 0 }
+	#validates :cost, presence: false, numericality: { greater_than_or_equal_to: 0 }
 
 	belongs_to :order
 	belongs_to :daily_menu_item
 
-	after_validation :calculate_cost
+	after_validation :set_cost
 
-	def cost
-		if quantity_vol.present? && quantity_vol > 0
-			(quantity_vol * daily_menu_item.price / daily_menu_item.menu_item.volume).round(2)
-		elsif quantity_por.present? && quantity_por > 0
+	def calculate_cost
+		if quantity_por.present?
 			quantity_por * daily_menu_item.price
+		elsif quantity_vol.present?
+			(quantity_vol * daily_menu_item.price / daily_menu_item.menu_item.volume).round(2)
 		end
 	end
 
-	def calculate_cost
+	def set_cost
+		puts 'cost_calc'
 		self.cost = if errors.empty?
-						cost
+						calculate_cost
 					else
 						nil
 					end
